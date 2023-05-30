db04b347ac494962899e409646646e748be1e624
mfroeh <mfroeh0@pm.me>
2023-05-30 22:44:55 +0200

    Experimental OpenMP frontier detection

--- ros2_ws/src/air_exploration/src/frontier.cpp
+++ ros2_ws/src/air_exploration/src/frontier.cpp
@@ -2,6 +2,7 @@
 
 #include <queue>
 #include <algorithm>
+#include <omp.h>
 
 // http://docs.ros.org/en/noetic/api/nav_msgs/html/msg/OccupancyGrid.html
 #define THRESHOLD 10
@@ -66,97 +67,107 @@ std::vector<Frontier> WFD(Map const &map, unsigned minsize)
     std::vector<Frontier> frontiers{};
     int outer{}, inner{};
 
-    // Outer BFS to find frontier points
-    while (!queue_m.empty())
+#pragma omp parallel
     {
-        outer++;
-        unsigned p{queue_m.front()};
-        queue_m.pop();
-
-        // If already visited
-        if (states[p] == MAP_CLOSE_LIST)
-            continue;
-
-        if (is_frontier(p, map))
+#pragma omp single
         {
-            std::queue<unsigned> queue_f{};
-            std::vector<unsigned> points{};
-            queue_f.push(p);
-            states[p] = FRONTIER_OPEN_LIST;
-
-            // Inner BFS to find adjacent frontier points
-            while (!queue_f.empty())
+            // Outer BFS to find frontier points
+            while (!queue_m.empty())
             {
-                inner++;
-                unsigned q{queue_f.front()};
-                queue_f.pop();
+                outer++;
+                unsigned p{queue_m.front()};
+                queue_m.pop();
 
-                // If already visited by outer BFS or belongs to other frontier
-                if (states[q] == MAP_CLOSE_LIST || states[q] == FRONTIER_CLOSE_LIST)
+                // If already visited
+                if (states[p] == MAP_CLOSE_LIST)
                     continue;
 
-                if (is_frontier(q, map))
+                if (is_frontier(p, map))
                 {
-                    points.push_back(q);
-                    unsigned adj_q[8];
-                    adj(q, map, adj_q);
-
-                    for (auto &w : adj_q)
+#pragma omp task
                     {
-                        // Not actually neighbors (>= map.size instead of < 0 because of unsigned)
-                        if (w >= map.size)
-                            continue;
+                        std::queue<unsigned> queue_f{};
+                        std::vector<unsigned> points{};
+                        queue_f.push(p);
+                        states[p] = FRONTIER_OPEN_LIST;
 
-                        if (states[w] != FRONTIER_OPEN_LIST && states[w] != FRONTIER_CLOSE_LIST && states[w] != MAP_CLOSE_LIST)
+                        // Inner BFS to find adjacent frontier points
+                        while (!queue_f.empty())
                         {
-                            if (map[w] != 100)
+                            inner++;
+                            unsigned q{queue_f.front()};
+                            queue_f.pop();
+
+                            // If already visited by outer BFS or belongs to other frontier
+                            if (states[q] == MAP_CLOSE_LIST || states[q] == FRONTIER_CLOSE_LIST)
+                                continue;
+
+                            if (is_frontier(q, map))
                             {
-                                queue_f.push(w);
-                                states[w] = FRONTIER_OPEN_LIST;
+                                points.push_back(q);
+                                unsigned adj_q[8];
+                                adj(q, map, adj_q);
+
+                                for (auto &w : adj_q)
+                                {
+                                    // Not actually neighbors (>= map.size instead of < 0 because of unsigned)
+                                    if (w >= map.size)
+                                        continue;
+
+                                    if (states[w] != FRONTIER_OPEN_LIST && states[w] != FRONTIER_CLOSE_LIST && states[w] != MAP_CLOSE_LIST)
+                                    {
+                                        if (map[w] != 100)
+                                        {
+                                            queue_f.push(w);
+                                            states[w] = FRONTIER_OPEN_LIST;
+                                        }
+                                    }
+                                }
+                            }
+                            states[q] = FRONTIER_CLOSE_LIST;
+                        }
+
+                        if (points.size() >= minsize)
+                            frontiers.push_back(Frontier{points, map});
+
+                        // Mark all points of new frontier as MAP_CLOSE_LIST
+                        for (auto &c : points)
+                            states[c] = MAP_CLOSE_LIST;
+                    }
+                }
+
+                unsigned adj_p[8];
+                adj(p, map, adj_p);
+                for (auto &v : adj_p)
+                {
+                    // Not actually neighbors (>= map.size instead of < 0 because of unsigned)
+                    if (v >= map.size)
+                        continue;
+
+                    // If not already queued, not visited by outer BFS, doesn't belong to frontier and has open space neighbors
+                    if (states[v] != MAP_OPEN_LIST && states[v] != MAP_CLOSE_LIST)
+                    {
+                        unsigned adj_v[8];
+                        adj(v, map, adj_v);
+                        // If v has atleast one open space neighbor
+                        for (auto &w : adj_v)
+                        {
+                            if (w >= map.size)
+                                continue;
+
+                            if (map[w] <= THRESHOLD && map[w] >= 0)
+                            {
+                                queue_m.push(v);
+                                states[v] = MAP_OPEN_LIST;
+                                break;
                             }
                         }
                     }
                 }
-                states[q] = FRONTIER_CLOSE_LIST;
-            }
-
-            if (points.size() >= minsize)
-                frontiers.push_back(Frontier{points, map});
-
-            // Mark all points of new frontier as MAP_CLOSE_LIST
-            for (auto &c : points)
-                states[c] = MAP_CLOSE_LIST;
-        }
-
-        unsigned adj_p[8];
-        adj(p, map, adj_p);
-        for (auto &v : adj_p)
-        {
-            // Not actually neighbors (>= map.size instead of < 0 because of unsigned)
-            if (v >= map.size)
-                continue;
-
-            // If not already queued, not visited by outer BFS, doesn't belong to frontier and has open space neighbors
-            if (states[v] != MAP_OPEN_LIST && states[v] != MAP_CLOSE_LIST)
-            {
-                unsigned adj_v[8];
-                adj(v, map, adj_v);
-                // If v has atleast one open space neighbor
-                for (auto &w : adj_v)
-                {
-                    if (w >= map.size)
-                        continue;
-
-                    if (map[w] <= THRESHOLD && map[w] >= 0)
-                    {
-                        queue_m.push(v);
-                        states[v] = MAP_OPEN_LIST;
-                        break;
-                    }
-                }
+                states[p] = MAP_CLOSE_LIST;
             }
         }
-        states[p] = MAP_CLOSE_LIST;
+        #pragma omp taskwait
     }
 
     return frontiers;
